c9665fb3d39f244ce9ae91c5a090a77a72a9fc34
andryyu <andryyu5419@163.com>
2017-10-10 23:25:50 +0800

    添加微信分享Activity

---
+++ android/app/src/main/java/com/readinggank/wxapi/WXEntryActivity.java
@@ -0,0 +1,19 @@
+package com.readinggank.wxapi;
+
+/**
+ * Created by yufei on 2017/10/10.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.theweflex.react.WeChatModule;
+
+public class WXEntryActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WeChatModule.handleIntent(getIntent());
+        finish();
+    }
+}
+
